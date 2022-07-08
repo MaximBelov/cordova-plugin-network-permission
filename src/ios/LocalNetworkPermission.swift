d68fce7981b57fc53d82fe2a5661e4bfef5696e9
Sascha Barrett Lenz <sascha.barrett.lenz@hexagon.com>
2022-07-08 16:01:58 +0200

    Use different implementation

--- src/ios/LocalNetworkPermission.swift
+++ src/ios/LocalNetworkPermission.swift
@@ -1,20 +1,15 @@
 @objc(LocalNetworkPermission) class LocalNetworkPermission : CDVPlugin {
-    var networkPermissionChecker: LocalNetworkPermissionChecker?
-
     @objc(checkPermissions:)
     func checkPermissions(_ command: CDVInvokedUrlCommand) {
-        networkPermissionChecker = LocalNetworkPermissionChecker(host: "255.255.255.255", port: 4567,
-        granted: {
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
-        },
-        failure: { error in
-            let errorMessage = error != nil ? error?.localizedDescription ?? nil : nil
-            if errorMessage != nil {
-                print("Permission check failed with error: \(errorMessage)")
-            }
+        // How to use
 
-            self.commandDelegate!.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: errorMessage), callbackId: command.callbackId)
-        })
+        LocalNetworkPrivacy().checkAccessState { granted in
+            if (granted) {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+                self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+            } else {
+                self.commandDelegate!.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Permission declined"), callbackId: command.callbackId)
+            }
+        }
     }
 }
