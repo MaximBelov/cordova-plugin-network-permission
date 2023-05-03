216308e0a557aced6443a529c55c344f598a1bda
Daniel Schneider <d.schneider@ipus.de>
2023-05-03 09:14:51 +0200

    Update Babel

--- www/localNetworkPermission.js
+++ www/localNetworkPermission.js
@@ -1,16 +1,11 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.requestLocalNetworkPermission = requestLocalNetworkPermission;
-
-var _exec = require('cordova/exec');
-
-var _exec2 = _interopRequireDefault(_exec);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+var _exec = _interopRequireDefault(require("cordova/exec"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function requestLocalNetworkPermission(successCallback, failureCallback) {
-    (0, _exec2.default)(successCallback, failureCallback, 'LocalNetworkPermission', 'checkPermissions');
+  (0, _exec["default"])(successCallback, failureCallback, 'LocalNetworkPermission', 'checkPermissions');
 }
